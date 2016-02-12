0670cb96d46087505a2920e51e9020f81dde36da
sensiasoft <alex.robin@sensiasoftware.com>
2016-02-12 23:06:58 +0100

    Initial version of Bluetooth LE device discovery module using D-Bus on Linux

---
+++ sensorhub-comm-ble-dbus/src/main/java/org/bluez/OutOfBand.java
@@ -0,0 +1,11 @@
+package org.bluez;
+import java.util.List;
+import org.freedesktop.dbus.DBusInterface;
+public interface OutOfBand extends DBusInterface
+{
+
+  public void AddRemoteData(String address, List<Byte> hash, List<Byte> randomizer);
+  public void RemoveRemoteData(String address);
+  public Pair<List<Byte>, List<Byte>> ReadLocalData();
+
+}
