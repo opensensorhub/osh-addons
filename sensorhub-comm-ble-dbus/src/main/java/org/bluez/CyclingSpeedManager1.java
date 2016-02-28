e3bcc7e4051b3df57464eb2cb9f39f8dccaf4030
sensiasoft <alex.robin@sensiasoftware.com>
2016-02-28 11:35:33 +0100

    Moved comm stuff to new osh-comm repo

--- sensorhub-comm-ble-dbus/src/main/java/org/bluez/CyclingSpeedManager1.java
+++
@@ -1,9 +0,0 @@
-package org.bluez;
-import org.freedesktop.dbus.DBusInterface;
-public interface CyclingSpeedManager1 extends DBusInterface
-{
-
-  public void RegisterWatcher(DBusInterface agent);
-  public void UnregisterWatcher(DBusInterface agent);
-
-}
--- sensorhub-comm-ble-dbus/src/main/java/org/bluez/Input1.java
+++
@@ -1,7 +0,0 @@
-package org.bluez;
-import org.freedesktop.dbus.DBusInterface;
-public interface Input1 extends DBusInterface
-{
-
-
-}
