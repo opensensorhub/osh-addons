12f3ef54379ae59f51b0e4b1957f543a61e21763
sensiasoft <alex.robin@sensiasoftware.com>
2016-03-25 16:23:54 -0500

    Updated Dahua driver to use new config classes + fixed JUnit tests

--- sensorhub-driver-dahua/src/main/java/org/sensorhub/impl/sensor/dahua/DahuaCameraConfig.java
+++ sensorhub-driver-dahua/src/main/java/org/sensorhub/impl/sensor/dahua/DahuaCameraConfig.java
@@ -15,25 +15,65 @@ Developer are Copyright (C) 2014 the Initial Developer. All Rights Reserved.
 
 package org.sensorhub.impl.sensor.dahua;
 
+import org.sensorhub.api.config.DisplayInfo;
 import org.sensorhub.api.sensor.SensorConfig;
-import org.sensorhub.impl.sensor.rtpcam.RTPCameraConfig;
+import org.sensorhub.impl.sensor.rtpcam.RTSPConfig;
+import org.sensorhub.impl.sensor.videocam.BasicVideoConfig;
+import org.sensorhub.impl.sensor.videocam.NetworkConfig;
+import org.sensorhub.impl.sensor.videocam.VideoResolution;
 
 
 /**
  * <p>
- * Implementation of sensor interface for generic Dahua Cameras using IP
- * protocol. This particular class stores configuration parameters.
+ * Configuration parameters for Dahua cameras
  * </p>
  * 
  * @author Mike Botts <mike.botts@botts-inc.com>
  * @since March 2016
  */
-public class DahuaCameraConfig extends RTPCameraConfig {
-	
-		
-	
-// TODO: Set variable for mounting (top up, top down, top sideways) or better set mounting angles relative to NED/ENU
-// ALSO, use flip=yes/no to flip image if necessary	
+public class DahuaCameraConfig extends SensorConfig
+{	
+    @DisplayInfo(label="Video", desc="Video settings")
+    public BasicVideoConfig video = new BasicVideoConfig();
+    
+    @DisplayInfo(label="Network", desc="Network configuration")
+    public NetworkConfig net = new NetworkConfig();
+    
+    @DisplayInfo(label="RTP/RTSP", desc="RTP/RTSP configuration")
+    public RTSPConfig rtsp = new RTSPConfig();
+    
+	// TODO: Set variable for mounting (top up, top down, top sideways) or better set mounting angles relative to NED/ENU
+    // ALSO, use flip=yes/no to flip image if necessary	
 	
 
+    public enum Resolution implements VideoResolution
+    {
+        D1("D1", 704, 480),
+        HD_720P("HD", 1280, 720),
+        HD_1080P("Full HD", 1920, 1080);
+        
+        private String text;
+        private int width, height;
+        
+        private Resolution(String text, int width, int height)
+        {
+            this.text = text;
+            this.width = width;
+            this.height = height;
+        }
+        
+        public int getWidth() { return width; };
+        public int getHeight() { return height; };
+        public String toString() { return text + " (" + width + "x" + height + ")"; }
+    };
+    
+    
+    public DahuaCameraConfig()
+    {
+        // default params for Dahua
+        video.resolution = Resolution.HD_720P;
+        rtsp.rtspPort = 554;
+        rtsp.videoPath = "/cam/realmonitor?channel=1&subtype=0";
+        rtsp.localUdpPort = 20000;
+    }
 }
