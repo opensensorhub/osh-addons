4374f46f1f13bc595df9c960ebfd2be42d95a0b0
mikebotts <mike.botts@botts-inc.com>
2016-04-21 15:23:03 -0500

    Support for Garmin VIRB and minor change to Dahua driver

---
+++ sensorhub-driver-virb-xe/src/main/java/org/sensorhub/impl/sensor/virbxe/VirbXeDriver.java
@@ -0,0 +1,309 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+The contents of this file are subject to the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+for the specific language governing rights and limitations under the License.
+ 
+Copyright (C) 2012-2015 Sensia Software LLC. All Rights Reserved.
+ 
+******************************* END LICENSE BLOCK ***************************/
+
+package org.sensorhub.impl.sensor.virbxe;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import net.opengis.sensorml.v20.IdentifierList;
+import net.opengis.sensorml.v20.PhysicalSystem;
+import net.opengis.sensorml.v20.SpatialFrame;
+import net.opengis.sensorml.v20.Term;
+
+import org.sensorhub.api.common.SensorHubException;
+import org.sensorhub.api.sensor.SensorException;
+import org.sensorhub.impl.security.ClientAuth;
+import org.sensorhub.impl.sensor.AbstractSensorModule;
+import org.vast.sensorML.SMLFactory;
+import org.vast.swe.SWEHelper;
+
+import com.google.gson.Gson;
+
+
+/**
+ * <p>
+ * Driver for Garmin VIRB XE camera
+ * </p>
+ *
+ * @author Mike Botts <mike.botts@botts-inc.com>
+ * @since April 12, 2016
+ */
+public class VirbXeDriver extends AbstractSensorModule<VirbXeConfig>
+{
+    protected final static String CRS_ID = "SENSOR_FRAME";
+        
+    // Navigation Data output
+    VirbXeNavOutput navDataInterface;
+    
+    // Video Data Output
+	//VirbXeOutput videoDataInterface;
+	
+    String hostName;
+    VirbXeConfig config;
+    
+    String serialNumber = " ";
+    String firmware = " ";
+    String modelNumber = " ";
+   
+    boolean doInit = true;
+    long connectionRetryPeriod = 2000L;
+    
+    
+    public VirbXeDriver()
+    {       
+    }
+
+
+    @Override
+    public void init(VirbXeConfig config) throws SensorHubException
+    {
+        super.init(config);
+
+        
+    }
+    
+    @Override
+    public void start() throws SensorHubException
+    {
+        hostName = config.net.remoteHost + "/virb";  
+        boolean done = false;
+        
+        
+        // check first if connected
+        while (waitForConnection(connectionRetryPeriod, config.connectTimeout) && !done)
+        {
+            // create output only the first time it is started
+            if (doInit)
+            {
+            	// TODO uncomment when videoDataInterface ready
+                // video output
+//                videoDataInterface = new DahuaVideoOutput(this);
+//                videoDataInterface.init();
+//                addOutput(videoDataInterface, false);
+        
+                // create navigation data interface
+                navDataInterface = new VirbXeNavOutput(this);
+                navDataInterface.init();
+                addOutput(navDataInterface, false);
+                
+                done = true;
+                doInit = false;
+            }
+            
+            // TODO uncomment when videoDataInterface ready
+//            videoDataInterface.start();
+            navDataInterface.start();
+            
+        }
+    }
+    
+
+
+    @Override
+    protected void updateSensorDescription()
+    {
+        synchronized (sensorDescription)
+        {
+            super.updateSensorDescription();
+            
+            SMLFactory smlFac = new SMLFactory();
+            sensorDescription.setId("Garmin_VIRB_XE_" + serialNumber);
+            sensorDescription.setUniqueIdentifier("urn:garmin:cam:" + serialNumber);
+            sensorDescription.setDescription("Garmin VIRB-XE camera with GPS and Orientation" );
+            
+            
+            IdentifierList ident = smlFac.newIdentifierList();
+            sensorDescription.getIdentificationList().add(ident);
+            Term term;
+            
+            term = smlFac.newTerm();
+            term.setDefinition(SWEHelper.getPropertyUri("Manufacturer"));
+            term.setLabel("Manufacturer Name");
+            term.setValue("Garmin");
+            ident.addIdentifier2(term);
+            
+            term = smlFac.newTerm();
+            term.setDefinition(SWEHelper.getPropertyUri("ModelNumber"));
+            term.setLabel("Model Number");
+            term.setValue(modelNumber);
+            ident.addIdentifier2(term);
+            
+            term = smlFac.newTerm();
+            term.setDefinition(SWEHelper.getPropertyUri("SerialNumber"));
+            term.setLabel("Serial Number");
+            term.setValue(serialNumber);
+            ident.addIdentifier2(term);
+            
+            term = smlFac.newTerm();
+            term.setDefinition(SWEHelper.getPropertyUri("Firmware"));
+            term.setLabel("Firmware");
+            term.setValue(firmware);
+            ident.addIdentifier2(term);
+            
+            // Long Name
+            term = smlFac.newTerm();
+            term.setDefinition(SWEHelper.getPropertyUri("LongName"));
+            term.setLabel("Long Name");
+            term.setValue("Garmin VIRB Video Camera " + modelNumber + ": " + serialNumber);
+            ident.addIdentifier2(term);
+        
+            // Short Name
+            term = smlFac.newTerm();
+            term.setDefinition(SWEHelper.getPropertyUri("ShortName"));
+            term.setLabel("Short Name");
+            term.setValue("Garmin VIRB: " + serialNumber);
+            ident.addIdentifier2(term);
+
+            
+            // TODO check this frame
+            SpatialFrame localRefFrame = smlFac.newSpatialFrame();
+            localRefFrame.setId(CRS_ID);
+            localRefFrame.setOrigin("Position of Accelerometers (as marked on the plastic box of the device)");
+            localRefFrame.addAxis("X", "The X axis is in the plane of the aluminum mounting plate, parallel to the serial connector (as marked on the plastic box of the device)");
+            localRefFrame.addAxis("Y", "The Y axis is in the plane of the aluminum mounting plate, orthogonal to the serial connector (as marked on the plastic box of the device)");
+            localRefFrame.addAxis("Z", "The Z axis is orthogonal to the aluminum mounting plate, so that the frame is direct (as marked on the plastic box of the device)");
+            ((PhysicalSystem)sensorDescription).addLocalReferenceFrame(localRefFrame);
+        }
+    }
+
+
+    @Override
+    public boolean isConnected()
+    {
+    	// Check connection to VIRB and get DeviceInfo
+    	// request JSON response, parse, and assign values
+     	String json = sendCommand("{\"command\":\"deviceInfo\"}");
+     	
+    	if (json.equalsIgnoreCase("0"))
+    		return false;
+  		
+    	// Returns an array with one component
+    	// serialize the DeviceInfo JSON Object
+    	Gson gson = new Gson(); 	
+      	DeviceInfo[] info = gson.fromJson(json, DeviceInfo[].class);
+    	  		
+    	modelNumber = info[0].model;
+    	serialNumber = info[0].deviceId;
+    	firmware = info[0].firmware;
+    	
+    	// set GPS on and set Units to metric
+    	//String response = 
+    	sendCommand("{\"command\":\"updateFeature\",\"feature\": \"gps\" ,\"value\": \"on\" }");
+    	//String response = 
+    	sendCommand("{\"command\":\"updateFeature\",\"feature\": \"units\" ,\"value\": \"Metric\" }");
+
+    	
+        return (true);
+    }
+    
+    // send Post command
+    public String sendCommand(String command){
+    	
+    	StringBuffer response = null;
+    	
+    	try
+    	{
+    		URL obj = new URL(hostName);
+    		HttpURLConnection con = (HttpURLConnection) obj.openConnection();    		
+    		con.setRequestMethod("POST");
+ 
+    		con.setDoOutput(true);
+    		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+    		wr.writeBytes(command);
+    		wr.flush();
+    		wr.close();
+
+    		// check response code for an error
+    		String responseCode = Integer.toString(con.getResponseCode());
+    		if ((responseCode.equalsIgnoreCase("-1")) || (responseCode.equalsIgnoreCase("401")))
+    			return "0";
+     		
+    		BufferedReader in = new BufferedReader(
+    		        new InputStreamReader(con.getInputStream()));
+    		String inputLine;
+    		response = new StringBuffer();
+
+    		while ((inputLine = in.readLine()) != null) {
+    			response.append(inputLine);
+    		}
+    		in.close();
+    		     		
+    	}
+    	catch (IOException e)
+    	{  		
+    		 e.printStackTrace();
+    	}
+    	
+    	return response.toString();
+    }
+   
+    
+   // Class to serialize JSON response
+   private class DeviceInfo{
+    	
+    	String model;
+    	String firmware;
+    	//String type;
+    	//String partNumber;
+    	String deviceId;
+    	  	
+    }
+
+    
+    @Override
+    protected void restartOnDisconnect()
+    {
+        super.restartOnDisconnect();
+    }
+
+
+    @Override
+    public void stop() throws SensorHubException
+    {
+        if (navDataInterface != null)
+            navDataInterface.stop();
+        
+//        if (videoDataInterface != null)
+//        	videoDataInterface.stop();
+                          
+    }
+    
+
+    @Override
+    public void cleanup() throws SensorHubException
+    {
+       
+    }
+    
+    
+    private void setAuth()
+    {
+        ClientAuth.getInstance().setUser(config.net.user);
+        if (config.net.password != null)
+            ClientAuth.getInstance().setPassword(config.net.password.toCharArray());
+    }
+
+
+    protected String getHostName()
+    {
+        setAuth();
+        return hostName;
+    } 
+    
+ }
---
+++ sensorhub-driver-virb-xe/src/main/java/org/sensorhub/impl/sensor/virbxe/VirbXeNavOutput.java
@@ -0,0 +1,356 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+The contents of this file are subject to the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+for the specific language governing rights and limitations under the License.
+ 
+Copyright (C) 2012-2015 Botts Innovative Research Inc. All Rights Reserved.
+ 
+******************************* END LICENSE BLOCK ***************************/
+
+package org.sensorhub.impl.sensor.virbxe;
+
+import org.sensorhub.impl.sensor.AbstractSensorOutput;
+import org.sensorhub.api.sensor.SensorDataEvent;
+import org.sensorhub.api.sensor.SensorException;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import net.opengis.swe.v20.DataBlock;
+import net.opengis.swe.v20.DataComponent;
+import net.opengis.swe.v20.DataEncoding;
+import net.opengis.swe.v20.DataType;
+import net.opengis.swe.v20.Quantity;
+import net.opengis.swe.v20.TextEncoding;
+import net.opengis.swe.v20.Vector;
+
+import org.vast.swe.SWEConstants;
+import org.vast.swe.SWEHelper;
+import org.vast.swe.helper.GeoPosHelper;
+
+import com.google.gson.Gson;
+
+
+/**
+ * <p>
+ * Implementation of the Garmin Navigation Output
+ * </p>
+ *
+ * @author Mike Botts
+ * @since April 14, 2016
+ */
+public class VirbXeNavOutput extends AbstractSensorOutput<VirbXeDriver>
+{
+    
+    DataComponent navData;
+    DataBlock navBlock;
+    TextEncoding textEncoding;
+    Timer timer;
+    
+    // set default timezone to GMT; check TZ in init below
+    TimeZone tz = TimeZone.getTimeZone("UTC");
+    DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");    
+
+    
+    public VirbXeNavOutput(VirbXeDriver parentSensor)
+    {
+        super(parentSensor);
+    }
+
+
+    @Override
+    public String getName()
+    {
+        return "navData";
+    }
+
+
+    @Override
+    protected void init() 
+    {
+        GeoPosHelper fac = new GeoPosHelper();
+        
+        // Common data record for all vectors
+        // build SWE Common record structure
+        navData = fac.newDataRecord(5);
+        navData.setName(getName());
+        navData.setDefinition("http://sensorml.com/ont/swe/property/stateVectors");
+        
+        // Time stamp
+        navData.addComponent("time", fac.newTimeStampIsoUTC());
+      
+        // Latitude, Longitude, Altitude
+        Vector locVector = fac.newLocationVectorLLA(SWEConstants.DEF_SENSOR_LOC);
+        locVector.setLabel("Location");
+        locVector.setDescription("Location measured by GPS device");
+        navData.addComponent("location", locVector);
+        
+        // set local reference frame
+        String localFrame = parentSensor.getUniqueIdentifier() + "#" + VirbXeDriver.CRS_ID;
+
+        // raw gyro inertial measurements
+        Vector angRate = fac.newAngularVelocityVector(
+                SWEHelper.getPropertyUri("AngularRate"),
+                localFrame, "deg/s");
+        angRate.setDataType(DataType.FLOAT);
+        navData.addComponent("gyro", angRate);
+
+        // Acceleration
+        Vector accel = fac.newAccelerationVector(
+                SWEHelper.getPropertyUri("Acceleration"),
+                localFrame, "m/s2");
+        accel.setDataType(DataType.FLOAT);
+        navData.addComponent("accel", accel);
+      
+        // Acceleration Magnitude 
+        Quantity accelMag = fac.newQuantity(SWEConstants.DEF_ACCELERATION_MAG, 
+        		"Acceleration Magnitude", 
+        		"Magnitude of the acceleration Vector", 
+        		"m/s2", DataType.FLOAT);
+         navData.addComponent("accelMag", accelMag);
+         
+         navBlock = navData.createDataBlock();
+        
+    }
+    
+    protected void start()
+    {
+
+    	if (timer != null)
+            return;
+        
+        try
+        {
+        	          
+          final DataBlock data = navBlock.renew();
+
+	    	
+	      TimerTask timerTask = new TimerTask()
+	      {
+	            @Override
+	            public void run()
+	            {
+	            	
+	                InputStream is = null;
+	                
+	                // send post query
+	                try
+	                {
+	                	
+	                	// request JSON response, parse, and assign values
+	                 	String json = getSensorData();
+	                 	
+	//                	if (json.equalsIgnoreCase("0"))
+	//                		return false;
+	              		
+	                    // set sampling time
+	                    double time = System.currentTimeMillis() / 1000.;
+	                    data.setDoubleValue(0, time);	                 	
+
+	                	// serialize the DeviceInfo JSON result object
+	                	Gson gson = new Gson(); 	
+	                  	SensorData[] sensors = gson.fromJson(json, SensorData[].class);
+	
+	                  	
+	                  	//  Identify each array component and assign to correct block index
+	                  	
+	                  	for (int i=0; i < sensors.length; i++)
+	                  	{
+	                  		// check for no data
+	                  		if ((sensors[i].has_data).equalsIgnoreCase("0"))
+	                  		{
+	                  			continue;
+	                  		}
+	                  		else if ((sensors[i].name).equalsIgnoreCase("Latitude"))
+	                        {
+	                            double val = Double.parseDouble(sensors[i].data);
+	                            data.setDoubleValue(1, val);
+	                        }
+	                  		else if ((sensors[i].name).equalsIgnoreCase("Longitude"))
+	                        {
+	                            double val = Double.parseDouble(sensors[i].data);
+	                            data.setDoubleValue(2, val);
+	                        }
+	                  		else if ((sensors[i].name).equalsIgnoreCase("Altitude"))
+	                        {
+	                            double val = Double.parseDouble(sensors[i].data);
+	                            data.setDoubleValue(3, val);
+	                        }
+	                  		else if ((sensors[i].name).equalsIgnoreCase("InternalGyroX"))
+	                        {
+	                            float val = Float.parseFloat(sensors[i].data);
+	                            data.setFloatValue(4, val);
+	                        }
+	                  		else if ((sensors[i].name).equalsIgnoreCase("InternalGyroY"))
+	                        {
+	                            float val = Float.parseFloat(sensors[i].data);
+	                            data.setFloatValue(5, val);
+	                        }
+	                  		else if ((sensors[i].name).equalsIgnoreCase("InternalGyroY"))
+	                        {
+	                            float val = Float.parseFloat(sensors[i].data);
+	                            data.setFloatValue(6, val);
+	                        }
+	                  		else if ((sensors[i].name).equalsIgnoreCase("InternalGyroZ"))
+	                        {
+	                            float val = Float.parseFloat(sensors[i].data);
+	                            data.setFloatValue(7, val);
+	                        }
+	                  		else if ((sensors[i].name).equalsIgnoreCase("InternalAccelX"))
+	                        {
+	                            float val = Float.parseFloat(sensors[i].data);
+	                            data.setFloatValue(8, val);
+	                        }
+	                  		else if ((sensors[i].name).equalsIgnoreCase("InternalAccelY"))
+	                        {
+	                            float val = Float.parseFloat(sensors[i].data);
+	                            data.setFloatValue(9, val);
+	                        }
+	                  		else if ((sensors[i].name).equalsIgnoreCase("InternalAccelZ"))
+	                        {
+	                            float val = Float.parseFloat(sensors[i].data);
+	                            data.setFloatValue(10, val);
+	                        }
+	                  		else if ((sensors[i].name).equalsIgnoreCase("InternalAccelG"))
+	                        {
+	                            float val = Float.parseFloat(sensors[i].data);
+	                            data.setFloatValue(11, val);
+	                        }
+	                			                  		
+			                latestRecord = data;
+			                latestRecordTime = System.currentTimeMillis();
+			                eventHandler.publishEvent(new SensorDataEvent(latestRecordTime, VirbXeNavOutput.this, latestRecord));
+	 	                  		
+	                  	}                	           	              	
+	                }
+	                catch (Exception e)
+	                {
+	                    e.printStackTrace();
+	                }
+	                finally
+	                {
+	                    // always close the stream even in case of error
+	                    try
+	                    {
+	                        if (is != null)
+	                            is.close();
+	                    }
+	                    catch (IOException e)
+	                    {
+	                    }
+	                }	
+	            }
+	        };
+
+	        timer = new Timer();
+	        timer.scheduleAtFixedRate(timerTask, 0, (long)(getAverageSamplingPeriod()*1000));
+	    }
+	    catch (Exception e)
+	    {
+	        // TODO Auto-generated catch block
+	        e.printStackTrace();
+	    }   	
+        
+    }
+    
+    public String getSensorData(){
+    	 
+    	StringBuffer response = null;
+    	
+    	try
+    	{
+            final URL urlVirb = new URL(parentSensor.getHostName() + "/virb");  
+    		
+    		HttpURLConnection con = (HttpURLConnection) urlVirb.openConnection();    		
+    		con.setRequestMethod("POST");
+ 
+    		String urlParameters = "{\"command\":\"sensors\"}";
+    		
+    		con.setDoOutput(true);
+    		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+    		wr.writeBytes(urlParameters);
+    		wr.flush();
+    		wr.close();
+
+    		// check response code for an error
+    		String responseCode = Integer.toString(con.getResponseCode());
+    		if ((responseCode.equalsIgnoreCase("-1")) || (responseCode.equalsIgnoreCase("401")))
+    			return "0";
+     		
+    		BufferedReader in = new BufferedReader(
+    		        new InputStreamReader(con.getInputStream()));
+    		String inputLine;
+    		response = new StringBuffer();
+
+    		while ((inputLine = in.readLine()) != null) {
+    			response.append(inputLine);
+    		}
+    		in.close();
+    		     		
+    	}
+    	catch (IOException e)
+    	{  		
+    		 e.printStackTrace();
+    	}
+    	
+    	return response.toString();
+    }
+    
+
+    // Class to serialize JSON response for "sensors"
+    private class SensorData{
+     	
+    	String name;
+//    	String type;
+    	String has_data;
+//    	String units;
+//    	String data_type;
+    	String data;  	
+     }
+    
+
+    protected void stop()
+    {
+    	if (timer != null)
+    	{
+	        timer.cancel();
+	        timer = null;
+    	}
+    }
+
+
+    @Override
+    public double getAverageSamplingPeriod()
+    {
+    	return 1.0;
+    }
+
+
+    @Override
+    public DataComponent getRecordDescription()
+    {
+        return navData;
+    }
+
+
+    @Override
+    public DataEncoding getRecommendedEncoding()
+    {
+        return textEncoding;
+    }
+}
