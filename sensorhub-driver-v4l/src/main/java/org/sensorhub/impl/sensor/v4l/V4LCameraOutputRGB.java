07f87e802924c63b0d96ff424fc80f1994b644fc
sensiasoft <alex.robin@sensiasoftware.com>
2016-04-23 15:26:53 -0500

    Mods to use videocam helper

--- sensorhub-driver-v4l/src/main/java/org/sensorhub/impl/sensor/v4l/V4LCameraOutputRGB.java
+++ sensorhub-driver-v4l/src/main/java/org/sensorhub/impl/sensor/v4l/V4LCameraOutputRGB.java
@@ -14,25 +14,13 @@ Copyright (C) 2012-2015 Sensia Software LLC. All Rights Reserved.
 
 package org.sensorhub.impl.sensor.v4l;
 
-import java.nio.ByteOrder;
-import net.opengis.swe.v20.BinaryEncoding;
-import net.opengis.swe.v20.ByteEncoding;
-import net.opengis.swe.v20.DataArray;
 import net.opengis.swe.v20.DataBlock;
 import net.opengis.swe.v20.DataComponent;
-import net.opengis.swe.v20.DataEncoding;
-import net.opengis.swe.v20.DataRecord;
-import net.opengis.swe.v20.DataType;
 import org.sensorhub.api.sensor.SensorDataEvent;
 import org.sensorhub.api.sensor.SensorException;
-import org.vast.data.BinaryComponentImpl;
-import org.vast.data.BinaryEncodingImpl;
-import org.vast.data.CountImpl;
-import org.vast.data.DataArrayImpl;
+import org.sensorhub.impl.sensor.videocam.VideoCamHelper;
 import org.vast.data.DataBlockByte;
-import org.vast.data.DataRecordImpl;
 import au.edu.jcu.v4l4j.CaptureCallback;
-import au.edu.jcu.v4l4j.RGBFrameGrabber;
 import au.edu.jcu.v4l4j.V4L4JConstants;
 import au.edu.jcu.v4l4j.VideoFrame;
 import au.edu.jcu.v4l4j.exceptions.V4L4JException;
@@ -48,7 +36,6 @@ import au.edu.jcu.v4l4j.exceptions.V4L4JException;
  */
 public class V4LCameraOutputRGB extends V4LCameraOutput implements CaptureCallback
 {
-    RGBFrameGrabber frameGrabber;
     DataComponent camDataStruct;
     
     
@@ -61,7 +48,7 @@ public class V4LCameraOutputRGB extends V4LCameraOutput implements CaptureCallba
     @Override
     public String getName()
     {
-        return "camOutput";
+        return "camOutput_RGB";
     }
     
     
@@ -81,17 +68,9 @@ public class V4LCameraOutputRGB extends V4LCameraOutput implements CaptureCallba
             camParams.frameRate = frameGrabber.getFrameInterval().denominator / frameGrabber.getFrameInterval().numerator;
             camParams.imgFormat = frameGrabber.getImageFormat().getName();
             
-            // build output structure
-            camDataStruct = new DataArrayImpl(camParams.imgHeight);
-            camDataStruct.setName(getName());
-            camDataStruct.setDefinition("http://sensorml.com/ont/swe/property/VideoFrame");
-            DataArray imgRow = new DataArrayImpl(camParams.imgWidth);
-            ((DataArray)camDataStruct).addComponent("row", imgRow);        
-            DataRecord imgPixel = new DataRecordImpl(3);
-            imgPixel.addComponent("red", new CountImpl(DataType.BYTE));
-            imgPixel.addComponent("green", new CountImpl(DataType.BYTE));
-            imgPixel.addComponent("blue", new CountImpl(DataType.BYTE));
-            imgRow.addComponent("pixel", imgPixel);
+            // create SWE output structure
+            VideoCamHelper fac = new VideoCamHelper();
+            dataStream = fac.newVideoOutputRGB(getName(), camParams.imgWidth, camParams.imgHeight);
             
             // start capture
             frameGrabber.setCaptureCallback(this);
@@ -102,19 +81,6 @@ public class V4LCameraOutputRGB extends V4LCameraOutput implements CaptureCallba
         {
             throw new SensorException("Error while initializing frame grabber", e);
         }        
-    }
-
-
-    @Override
-    protected void stop()
-    {
-        if (frameGrabber != null)
-        {
-            if (parentSensor.camParams.doCapture)
-                frameGrabber.stopCapture();
-            parentSensor.videoDevice.releaseFrameGrabber();
-            frameGrabber = null;
-        }
     }
     
     
@@ -150,32 +116,4 @@ public class V4LCameraOutputRGB extends V4LCameraOutput implements CaptureCallba
             e.printStackTrace();
         }    
     }
-    
-    
-    @Override
-    public DataEncoding getRecommendedEncoding()
-    {
-        BinaryEncoding dataEnc = new BinaryEncodingImpl();
-        dataEnc.setByteEncoding(ByteEncoding.RAW);
-        dataEnc.setByteOrder(ByteOrder.BIG_ENDIAN);
-        dataEnc.addMemberAsComponent(new BinaryComponentImpl("row/pixel/red", DataType.BYTE));
-        dataEnc.addMemberAsComponent(new BinaryComponentImpl("row/pixel/green", DataType.BYTE));
-        dataEnc.addMemberAsComponent(new BinaryComponentImpl("row/pixel/blue", DataType.BYTE));
-        return dataEnc;
-    }
-
-
-    @Override
-    public double getAverageSamplingPeriod()
-    {
-        return parentSensor.camParams.frameRate;
-    }
-
-
-    @Override
-    public DataComponent getRecordDescription()
-    {
-        return camDataStruct;
-    }
-
 }
