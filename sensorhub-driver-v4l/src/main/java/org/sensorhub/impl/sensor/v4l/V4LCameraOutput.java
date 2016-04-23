99278498f49a99f0c234d458d4804b9b3c608037
sensiasoft <alex.robin@sensiasoftware.com>
2016-04-23 15:26:53 -0500

    Mods to use videocam helper

--- sensorhub-driver-v4l/src/main/java/org/sensorhub/impl/sensor/v4l/V4LCameraOutput.java
+++ sensorhub-driver-v4l/src/main/java/org/sensorhub/impl/sensor/v4l/V4LCameraOutput.java
@@ -14,12 +14,26 @@ Copyright (C) 2012-2016 Sensia Software LLC. All Rights Reserved.
 
 package org.sensorhub.impl.sensor.v4l;
 
+import net.opengis.swe.v20.DataComponent;
+import net.opengis.swe.v20.DataEncoding;
+import net.opengis.swe.v20.DataStream;
 import org.sensorhub.api.sensor.SensorException;
 import org.sensorhub.impl.sensor.AbstractSensorOutput;
+import au.edu.jcu.v4l4j.FrameGrabber;
 
 
+/**
+ * <p>
+ * Common base for all video outputs (different codecs)
+ * </p>
+ *
+ * @author Alex Robin
+ * @since Apr 23, 2016
+ */
 public abstract class V4LCameraOutput extends AbstractSensorOutput<V4LCameraDriver>
 {
+    DataStream dataStream;
+    FrameGrabber frameGrabber;
     long systemTimeOffset = -1L;
     
     
@@ -37,8 +51,45 @@ public abstract class V4LCameraOutput extends AbstractSensorOutput<V4LCameraDriv
 
     protected abstract void init() throws SensorException;
 
+    
+    @Override
+    protected void stop()
+    {
+        if (frameGrabber != null)
+        {
+            if (parentSensor.camParams.doCapture)
+            {
+                parentSensor.getLogger().debug("Stopping V4L capture");
+                frameGrabber.stopCapture();
+                parentSensor.getLogger().debug("V4L capture stopped");
+            }
+            
+            parentSensor.videoDevice.releaseFrameGrabber();
+            parentSensor.videoDevice.releaseControlList();            
+            frameGrabber = null;
+        }
+    }
+        
+    
+    @Override
+    public DataEncoding getRecommendedEncoding()
+    {
+        return dataStream.getEncoding();
+    }
 
-    protected abstract void stop();
+
+    @Override
+    public double getAverageSamplingPeriod()
+    {
+        return parentSensor.camParams.frameRate;
+    }
+
+
+    @Override
+    public DataComponent getRecordDescription()
+    {
+        return dataStream.getElementType();
+    }
     
     
     protected final double getJulianTimeStamp(long v4lTimeStampMicros)
