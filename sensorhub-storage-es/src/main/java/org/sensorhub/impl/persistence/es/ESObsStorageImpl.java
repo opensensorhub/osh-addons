a47591e7c889fb2d816668fd188be92039b5db47
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2017-02-20 14:26:31 +0100

    Add implementation of the data record storage

--- sensorhub-storage-es/src/main/java/org/sensorhub/impl/persistence/es/ESObsStorageImpl.java
+++ sensorhub-storage-es/src/main/java/org/sensorhub/impl/persistence/es/ESObsStorageImpl.java
@@ -39,6 +39,9 @@ import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.sort.SortOrder;
@@ -54,7 +57,6 @@ import org.sensorhub.api.persistence.IRecordStoreInfo;
 import org.sensorhub.api.persistence.IStorageModule;
 import org.sensorhub.api.persistence.StorageException;
 import org.sensorhub.impl.module.AbstractModule;
-import org.sensorhub.impl.persistence.h2.DataStreamInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.vast.util.Bbox;
@@ -77,6 +79,14 @@ import net.opengis.swe.v20.DataEncoding;
  * @since 2017
  */
 public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements IObsStorageModule<ESStorageConfig> {
+	private static final String RECORD_TYPE_FIELD_NAME = "recordType";
+
+	private static final String PRODUCER_ID_FIELD_NAME = "producerID";
+
+	private static final int DOUBLE_TO_LONG_MULTIPLIER = 1000;
+
+	private static final String RS_KEY_SEPARATOR = "$__$";
+
 	private static final String BLOB_FIELD_NAME = "blob";
 
 	private static final String TIMESTAMP_FIELD_NAME = "timestamp";
@@ -200,7 +210,7 @@ public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements
 	public AbstractProcess getLatestDataSourceDescription() {
 		// build search response given a timestamp desc sort
 		SearchResponse response = processDescSearch
-							.addSort("timestamp", SortOrder.DESC)
+							.addSort(TIMESTAMP_FIELD_NAME, SortOrder.DESC)
 							.get();
 		
 		AbstractProcess result = null;
@@ -222,7 +232,8 @@ public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements
 		// the response is applied a post filter allowing to specify a range request on the timestamp
 		// the hits should be directly filtered
 		SearchResponse response = processDescSearch
-				.setQuery(QueryBuilders.rangeQuery("timestamp").from(startTime).to(endTime))                // Query
+				.setQuery(QueryBuilders.rangeQuery(TIMESTAMP_FIELD_NAME)
+						.from((long)(startTime*DOUBLE_TO_LONG_MULTIPLIER)).to((long)(endTime*DOUBLE_TO_LONG_MULTIPLIER)))     // Query
 		        .get();
 		
 		// the corresponding filtering hits
@@ -238,10 +249,10 @@ public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements
 		AbstractProcess result = null;
 		
 		// build the request
-		GetRequest getRequest = new GetRequest( "uuid1","desc",time+"");
+		GetRequest getRequest = new GetRequest( getLocalID(),DESC_HISTORY_IDX_NAME,(long)(time*DOUBLE_TO_LONG_MULTIPLIER)+"");
 		
 		// build  and execute the response
-		GetResponse response = client.get( getRequest ).actionGet();
+		GetResponse response = client.get(getRequest).actionGet();
 		
 		// if any response
 		if (response.isExists()) {
@@ -255,13 +266,15 @@ public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements
 	}
 
 	protected boolean storeDataSourceDescription(AbstractProcess process, double time, boolean update) {
+		long timestampAsLong = (long) (time*DOUBLE_TO_LONG_MULTIPLIER);
+		
 		Map<String, Object> json = new HashMap<String, Object>();
-		json.put(TIMESTAMP_FIELD_NAME,time);
+		json.put(TIMESTAMP_FIELD_NAME,timestampAsLong);
 		json.put(BLOB_FIELD_NAME,ESObsStorageImpl.getBlob(process));
 		
 		if (update) {
 			// prepare update
-			UpdateRequest updateRequest = new UpdateRequest(getLocalID(), DESC_HISTORY_IDX_NAME, time+"");
+			UpdateRequest updateRequest = new UpdateRequest(getLocalID(), DESC_HISTORY_IDX_NAME, timestampAsLong+"");
 			updateRequest.doc(json);
 			
 			String id=null;
@@ -315,7 +328,7 @@ public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements
 
 	@Override
 	public void removeDataSourceDescription(double time) {
-		DeleteRequest deleteRequest = new DeleteRequest(getLocalID(), DESC_HISTORY_IDX_NAME, time+"");
+		DeleteRequest deleteRequest = new DeleteRequest(getLocalID(), DESC_HISTORY_IDX_NAME, (long)(time*DOUBLE_TO_LONG_MULTIPLIER)+"");
 		try {
 			client.delete(deleteRequest).get().getId();
 		} catch (InterruptedException | ExecutionException e) {
@@ -325,12 +338,12 @@ public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements
 
 	@Override
 	public void removeDataSourceDescriptionHistory(double startTime, double endTime) {
-		System.err.println("TODO: removeDataSourceDescriptionHistory");
 		// query ES to get the corresponding timestamp
 		// the response is applied a post filter allowing to specify a range request on the timestamp
 		// the hits should be directly filtered
 		SearchResponse response = processDescSearch
-				.setPostFilter(QueryBuilders.rangeQuery("timestamp").from(startTime).to(endTime))                // Query
+				.setQuery(QueryBuilders.rangeQuery(TIMESTAMP_FIELD_NAME)
+					.from((long)(startTime*DOUBLE_TO_LONG_MULTIPLIER)).to((long)endTime*DOUBLE_TO_LONG_MULTIPLIER)) // Query
 		        .get();
 		
 		// the corresponding filtering hits
@@ -368,9 +381,14 @@ public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements
 		Object blob = ESObsStorageImpl.getBlob(rsInfo);
 		
 		// set id and blob before executing the request
-		recordStoreInfoIdx.setId(name).setSource(blob).get();
+		String id = recordStoreInfoIdx.setId(name).setSource(blob).get().getId();
 		
 		//TODO: make the link to the recordStore storage
+		// either we can use an intermediate mapping table or use directly the recordStoreInfo index
+		// to fetch the corresponding description
+		
+		// To test: try to use the recordStoreInfo index directly
+		// do nothing 
 	}
 
 	@Override
@@ -383,8 +401,32 @@ public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements
 
 	@Override
 	public double[] getRecordsTimeRange(String recordType) {
-		System.err.println("TODO: getRecordsTimeRange");
-		return null;
+		SearchResponse response = processDescSearch
+				.setQuery(QueryBuilders.termQuery(RECORD_TYPE_FIELD_NAME, recordType))
+				.addSort(TIMESTAMP_FIELD_NAME, SortOrder.DESC) // sort results by DESC timestamp
+		        .get();
+		
+		double[] result = null;
+		long totalHits = response.getHits().getTotalHits();
+		
+		// if at least 2 hits
+		if(totalHits >= 2) {
+			// get the first one and the last one
+			//get the timestamp of the first hit
+			long storedTimestamp = (long) response.getHits().getAt(0).getSource().get(TIMESTAMP_FIELD_NAME);
+			
+			// convert long to double
+			double firstTimestamp = storedTimestamp / DOUBLE_TO_LONG_MULTIPLIER;
+			
+			// get the timestamp of the last hit
+			storedTimestamp = (long) response.getHits().getAt((int) (totalHits-1)).getSource().get(TIMESTAMP_FIELD_NAME);
+			// convert long to double
+			double lastTimestamp = storedTimestamp / DOUBLE_TO_LONG_MULTIPLIER;
+			
+			// store the result
+			result = new double[]{firstTimestamp,lastTimestamp};
+		}
+		return result;
 	}
 
 	@Override
@@ -395,20 +437,125 @@ public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements
 
 	@Override
 	public DataBlock getDataBlock(DataKey key) {
-		System.err.println("TODO: getDataBlock");
-		return null;
+		// build the key as recordTYpe_timestamp_producerID
+		String esKey = getRsKey(key);
+		
+		// build the request
+		GetRequest getRequest = new GetRequest( getLocalID(),RS_DATA_IDX_NAME,esKey);
+		
+		// build  and execute the response
+		GetResponse response = client.get(getRequest).actionGet();
+		
+		DataBlock result = null;
+		
+		// deserialize the blob field from the response if any
+		if(response.isExists()) {
+			result = ESObsStorageImpl.<DataBlock>getObject(response.getSource().get(BLOB_FIELD_NAME)); // DataBlock
+		}
+		return result;
 	}
 
 	@Override
 	public Iterator<DataBlock> getDataBlockIterator(IDataFilter filter) {
-		System.err.println("TODO: getDataBlockIterator");
-		return null;
+		// build filter
+		long startTimestamp = (long)(filter.getTimeStampRange()[0]*DOUBLE_TO_LONG_MULTIPLIER);
+		long endTimestamp = (long)(filter.getTimeStampRange()[1]*DOUBLE_TO_LONG_MULTIPLIER);
+		
+		// prepare filter
+		QueryBuilder timeStampRangeQuery = QueryBuilders.rangeQuery(TIMESTAMP_FIELD_NAME).from(startTimestamp).to(endTimestamp);
+		QueryBuilder recordTypeQuery = QueryBuilders.termQuery(RECORD_TYPE_FIELD_NAME, filter.getRecordType());
+		QueryBuilder producerID = QueryBuilders.termQuery(PRODUCER_ID_FIELD_NAME, filter.getProducerIDs());
+		
+		// aggregate queries
+		BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery()
+				.must(timeStampRangeQuery)
+				.must(recordTypeQuery)
+				.must(producerID);
+		
+		// build response
+		final SearchResponse scrollResp = recordStoreSearch
+		        .setScroll(new TimeValue(config.pingTimeout))
+		        .setQuery(queryBuilder)
+		        .setSize(config.scrollFetchSize).get(); //max of scrollFetchSize hits will be returned for each scroll
+		
+		// get iterator from response
+		final Iterator<SearchHit> searchHitsIterator = scrollResp.getHits().iterator();
+		
+		// build a datablock iterator based on the searchHits iterator
+		return new Iterator<DataBlock>(){
+
+			@Override
+			public boolean hasNext() {
+				return searchHitsIterator.hasNext();
+			}
+
+			@Override
+			public DataBlock next() {
+				SearchHit nextSearchHit = searchHitsIterator.next();
+				// get DataBlock from blob
+				return ESObsStorageImpl.<DataBlock>getObject(nextSearchHit.getSource().get(BLOB_FIELD_NAME)); // DataBlock
+			}
+		};
 	}
 
 	@Override
 	public Iterator<? extends IDataRecord> getRecordIterator(IDataFilter filter) {
-		System.err.println("TODO: getRecordIterator");
-		return null;
+		// build filter
+		long startTimestamp = (long)(filter.getTimeStampRange()[0]*DOUBLE_TO_LONG_MULTIPLIER);
+		long endTimestamp = (long)(filter.getTimeStampRange()[1]*DOUBLE_TO_LONG_MULTIPLIER);
+		
+		// prepare filter
+		QueryBuilder timeStampRangeQuery = QueryBuilders.rangeQuery(TIMESTAMP_FIELD_NAME).from(startTimestamp).to(endTimestamp);
+		QueryBuilder recordTypeQuery = QueryBuilders.termQuery(RECORD_TYPE_FIELD_NAME, filter.getRecordType());
+		QueryBuilder producerID = QueryBuilders.termQuery(PRODUCER_ID_FIELD_NAME, filter.getProducerIDs());
+		
+		// aggregate queries
+		BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery()
+				.must(timeStampRangeQuery)
+				.must(recordTypeQuery)
+				.must(producerID);
+		
+		// build response
+		final SearchResponse scrollResp = recordStoreSearch
+		        .setScroll(new TimeValue(config.pingTimeout))
+		        .setQuery(queryBuilder)
+		        .setSize(config.scrollFetchSize).get(); //max of scrollFetchSize hits will be returned for each scroll
+		
+		// get iterator from response
+		final Iterator<SearchHit> searchHitsIterator = scrollResp.getHits().iterator();
+		// build a datablock iterator based on the searchHits iterator
+		
+		return new Iterator<IDataRecord>(){
+
+			@Override
+			public boolean hasNext() {
+				return searchHitsIterator.hasNext();
+			}
+
+			@Override
+			public IDataRecord next() {
+				SearchHit nextSearchHit = searchHitsIterator.next();
+				
+				// build key
+				final DataKey key = getDataKey(nextSearchHit.getId());
+				// get DataBlock from blob
+				final DataBlock datablock=ESObsStorageImpl.<DataBlock>getObject(nextSearchHit.getSource().get(BLOB_FIELD_NAME)); // DataBlock
+				
+				return new IDataRecord(){
+
+					@Override
+					public DataKey getKey() {
+						return key;
+					}
+
+					@Override
+					public DataBlock getData() {
+						return datablock;
+					}
+					
+				};
+			}
+		};
 	}
 
 	@Override
@@ -419,22 +566,106 @@ public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements
 
 	@Override
 	public void storeRecord(DataKey key, DataBlock data) {
-		System.err.println("TODO: storeRecord");
+		// build the key as recordTYpe_timestamp_producerID
+		String esKey = getRsKey(key);
+		
+		// get blob from dataBlock object using serializer
+		Object blob = ESObsStorageImpl.getBlob(data);
+		
+		Map<String, Object> json = new HashMap<String, Object>();
+		json.put(TIMESTAMP_FIELD_NAME,key.timeStamp); // store timestamp
+		json.put(PRODUCER_ID_FIELD_NAME,key.producerID); // store producerID
+		json.put(RECORD_TYPE_FIELD_NAME,key.recordType); // store recordType
+		json.put(BLOB_FIELD_NAME,ESObsStorageImpl.getBlob(blob)); // store DataBlock
+		
+		// set id and blob before executing the request
+		String id = recordStoreIdx.setId(esKey).setSource(json).get().getId();
 	}
 
 	@Override
 	public void updateRecord(DataKey key, DataBlock data) {
-		System.err.println("TODO: updateRecord");
+		// build the key as recordTYpe_timestamp_producerID
+		String esKey = getRsKey(key);
+		
+		// get blob from dataBlock object using serializer
+		Object blob = ESObsStorageImpl.getBlob(data);
+		
+		//TOCHECK: do we need to store the whole key?
+		Map<String, Object> json = new HashMap<String, Object>();
+		json.put(TIMESTAMP_FIELD_NAME,key.timeStamp); // store timestamp
+		json.put(PRODUCER_ID_FIELD_NAME,key.producerID); // store producerID
+		json.put(RECORD_TYPE_FIELD_NAME,key.recordType); // store recordType
+		json.put(BLOB_FIELD_NAME,ESObsStorageImpl.getBlob(blob)); // store DataBlock
+		
+		// prepare update
+		UpdateRequest updateRequest = new UpdateRequest(getLocalID(), RS_DATA_IDX_NAME, esKey);
+		updateRequest.doc(json);
+		
+		String id=null;
+		try {
+			id = client.update(updateRequest).get().getId();
+		} catch (InterruptedException | ExecutionException e) {
+			log.error("[ES] Cannot update the object with the key: "+esKey);
+		}
 	}
 
 	@Override
 	public void removeRecord(DataKey key) {
-		System.err.println("TODO: removeRecord");
+		// build the key as recordTYpe_timestamp_producerID
+		String esKey = getRsKey(key);
+		
+		// prepare delete request
+		DeleteRequest deleteRequest = new DeleteRequest(getLocalID(), RS_DATA_IDX_NAME, esKey);
+		try {
+			// execute delete
+			client.delete(deleteRequest).get().getId();
+		} catch (InterruptedException | ExecutionException e) {
+			log.error("[ES] Cannot delete the object with the key: "+esKey);
+		}
 	}
 
 	@Override
 	public int removeRecords(IDataFilter filter) {
-		System.err.println("TODO: removeRecords");
+		// get filter
+		long startTimestamp = (long)(filter.getTimeStampRange()[0]*DOUBLE_TO_LONG_MULTIPLIER);
+		long endTimestamp = (long)(filter.getTimeStampRange()[1]*DOUBLE_TO_LONG_MULTIPLIER);
+		
+		// MultiSearch API does not support scroll?!
+		// prepare filter
+		QueryBuilder timeStampRangeQuery = QueryBuilders.rangeQuery(TIMESTAMP_FIELD_NAME).from(startTimestamp).to(endTimestamp);
+		QueryBuilder recordTypeQuery = QueryBuilders.termQuery(RECORD_TYPE_FIELD_NAME, filter.getRecordType());
+		QueryBuilder producerID = QueryBuilders.termQuery(PRODUCER_ID_FIELD_NAME, filter.getProducerIDs());
+		
+		// aggregate queries
+		BoolQueryBuilder queryBuilder = QueryBuilders.boolQuery()
+				.must(timeStampRangeQuery)
+				.must(recordTypeQuery)
+				.must(producerID);
+		
+		// build response
+		SearchResponse scrollResp = recordStoreSearch
+		        .setScroll(new TimeValue(config.pingTimeout))
+		        .setQuery(queryBuilder)
+		        .setSize(config.scrollFetchSize).get(); //max of scrollFetchSize hits will be returned for each scroll
+		//Scroll until no hits are returned
+		int nb = 0;
+		do {
+		    for (SearchHit hit : scrollResp.getHits().getHits()) {
+		    	// prepare delete request
+				DeleteRequest deleteRequest = new DeleteRequest(getLocalID(),RS_DATA_IDX_NAME, hit.getId());
+				// execute delete
+				try {
+					client.delete(deleteRequest).get().getId();
+				} catch (InterruptedException | ExecutionException e) {
+					log.error("[ES] Cannot delete the object with the key: "+hit.getId());
+				}
+		    	nb++;
+		    }
+
+		    scrollResp = client.prepareSearchScroll(scrollResp.getScrollId()).setScroll(new TimeValue(config.scrollMaxDuration)).execute().actionGet();
+		} while(scrollResp.getHits().getHits().length != 0); // Zero hits mark the end of the scroll and the while loop.
+		
+		log.info("[ES] Delete "+nb+" records from "+new Date(startTimestamp)+" to "+new Date(endTimestamp));
 		return 0;
 	}
 
@@ -476,5 +707,32 @@ public class ESObsStorageImpl extends AbstractModule<ESStorageConfig> implements
 	//TODO: use Kryo
 	private static  <T> T getObject(Object blob) {
 		return null;
+	}
+	
+	/**
+	 * Transform a DataKey into an ES key as: <recordtype><SEPARATOR><timestamp><SEPARATOR><producerID>.
+	 * @param key the ES key.
+	 * @return the ES key. 
+	 */
+	protected String getRsKey(DataKey key) {
+		return key.recordType+RS_KEY_SEPARATOR+(long)(key.timeStamp*DOUBLE_TO_LONG_MULTIPLIER)+RS_KEY_SEPARATOR+key.producerID;
+	}
+	
+	/**
+	 * Transform the recordStorage data key into a DataKey by splitting <recordtype><SEPARATOR><timestamp><SEPARATOR><producerID>.
+	 * @param dataKey the corresponding dataKey
+	 * @return the dataKey. NULL if the length != 3 after splitting
+	 */
+	protected DataKey getDataKey(String rsKey) {
+		DataKey dataKey = null;
+		
+		// split the rsKey using separator
+		String [] split = rsKey.split(RS_KEY_SEPARATOR);
+		
+		// must find <recordtype><SEPARATOR><timestamp><SEPARATOR><producerID>
+    	if(split.length == 3) {
+    		dataKey = new DataKey(split[0], split[2], Long.parseLong(split[1])/(double)DOUBLE_TO_LONG_MULTIPLIER);
+    	}
+		return dataKey;
 	}
 }
