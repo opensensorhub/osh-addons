730a301d4b5b6bbd2b1492cd4cd8ae740c341ec1
Mathieu Dhainaut <mathieu.dhainaut@gmail.com>
2017-02-24 01:27:55 +0100

    Add junit for ObsStorage

--- sensorhub-storage-es/src/test/java/org/sensorhub/impl/persistence/es/TestEsBasicStorage.java
+++ sensorhub-storage-es/src/test/java/org/sensorhub/impl/persistence/es/TestEsBasicStorage.java
@@ -14,46 +14,65 @@ Copyright (C) 2012-2015 Sensia Software LLC. All Rights Reserved.
 
 package org.sensorhub.impl.persistence.es;
 
+import java.net.InetAddress;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.common.transport.TransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.sensorhub.api.common.SensorHubException;
 import org.sensorhub.test.persistence.AbstractTestBasicStorage;
 
+public class TestEsBasicStorage extends AbstractTestBasicStorage<ESBasicStorageImpl> {
 
-public class TestEsBasicStorage extends AbstractTestBasicStorage<ESBasicStorageImpl>
-{
-    
-    @Before
-    public void init() throws Exception
-    {
-        ESBasicStorageConfig config = new ESBasicStorageConfig();
-        config.autoStart = true;
-        config.storagePath = "elastic-cluster";
-        List<String> nodes = new ArrayList<String>();
-        nodes.add("localhost:9300");
-        
-        config.nodeUrls = nodes;
-        config.scrollFetchSize = 2;
-        config.id = "junit_"+UUID.randomUUID().toString();
-        storage = new ESBasicStorageImpl();
-        storage.init(config);
-        storage.start();
-    }
-    
+	@Before
+	public void init() throws Exception {
+		ESBasicStorageConfig config = new ESBasicStorageConfig();
+		config.autoStart = true;
+		config.storagePath = "elastic-cluster";
+		List<String> nodes = new ArrayList<String>();
+		nodes.add("localhost:9300");
 
-    @Override
-    protected void forceReadBackFromStorage() throws Exception
-    {
-    	storage.commit();
-    }
-    
-    
-    @After
-    public void cleanup()
-    {
-    }
-    
+		config.nodeUrls = nodes;
+		config.scrollFetchSize = 2;
+		config.id = "junit_" + UUID.randomUUID().toString();
+		storage = new ESBasicStorageImpl();
+		storage.init(config);
+		storage.start();
+	}
+
+	@Override
+	protected void forceReadBackFromStorage() throws Exception {
+		storage.commit();
+	}
+
+	@AfterClass
+	public static void cleanup() throws UnknownHostException {
+		// add transport address(es)
+		Settings settings = Settings.builder()
+		        .put("cluster.name", "elastic-cluster").build();
+		TransportClient client = new PreBuiltTransportClient(settings)
+		        .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+					
+		String idxName = "junit_*";
+
+		DeleteIndexResponse delete = client.admin().indices().delete(new DeleteIndexRequest(idxName)).actionGet();
+		if (!delete.isAcknowledged()) {
+			System.err.println("Index wasn't deleted");
+		}
+		
+		client.close();
+	}
 }
---
+++ sensorhub-storage-es/src/test/java/org/sensorhub/impl/persistence/es/TestEsObsStorage.java
@@ -0,0 +1,60 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+The contents of this file are subject to the Mozilla Public License, v. 2.0.
+If a copy of the MPL was not distributed with this file, You can obtain one
+at http://mozilla.org/MPL/2.0/.
+
+Software distributed under the License is distributed on an "AS IS" basis,
+WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+for the specific language governing rights and limitations under the License.
+ 
+Copyright (C) 2012-2015 Sensia Software LLC. All Rights Reserved.
+ 
+******************************* END LICENSE BLOCK ***************************/
+
+package org.sensorhub.impl.persistence.es;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.After;
+import org.junit.Before;
+import org.sensorhub.test.persistence.AbstractTestObsStorage;
+
+
+public class TestEsObsStorage extends AbstractTestObsStorage<ESObsStorageImpl>
+{
+    
+    @Before
+    public void init() throws Exception
+    {
+        ESBasicStorageConfig config = new ESBasicStorageConfig();
+        config.autoStart = true;
+        config.storagePath = "elastic-cluster";
+        List<String> nodes = new ArrayList<String>();
+        nodes.add("localhost:9300");
+        
+        config.nodeUrls = nodes;
+        config.scrollFetchSize = 2;
+        config.scrollMaxDuration = 999999000;
+        config.id = "junit_"+UUID.randomUUID().toString();
+        storage = new ESObsStorageImpl();
+        storage.init(config);
+        storage.start();
+    }
+    
+
+    @Override
+    protected void forceReadBackFromStorage() throws Exception
+    {
+    	storage.commit();
+    }
+    
+    
+    @After
+    public void cleanup()
+    {
+    }
+    
+}
