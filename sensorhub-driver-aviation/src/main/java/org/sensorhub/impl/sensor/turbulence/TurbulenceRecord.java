cd57c3089cec0156b5334021d13434c80b4ba16b
Tony Cook <tony@anythingweather.com>
2017-10-09 16:26:26 -0500

    Removed Turbulence files which are now part of FlightAware driver

--- sensorhub-driver-aviation/src/main/java/org/sensorhub/impl/sensor/turbulence/TurbulenceRecord.java
+++
@@ -1,9 +0,0 @@
-package org.sensorhub.impl.sensor.turbulence;
-
-public class TurbulenceRecord
-{
-	long time;  // constant for now but will be variable if we incorporate forecast turb
-	double lat;
-	double lon;
-	float [] turbulence;
-}
