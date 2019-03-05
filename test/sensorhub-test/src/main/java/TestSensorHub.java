6dcdd028a47aa82bfd43c63d93d9527e10333698
Alex Robin <alex.robin@sensiasoft.com>
2019-03-05 11:52:56 +0100

    Added test project allowing to launch OSH with the desired add-ons

---
+++ test/sensorhub-test/src/main/java/TestSensorHub.java
@@ -0,0 +1,42 @@
+import org.sensorhub.impl.SensorHub;
+
+
+public class TestSensorHub
+{
+    private TestSensorHub()
+    {        
+    }
+    
+    
+    public static void main(String[] args) throws Exception
+    {
+        //SensorHub.main(new String[] {"src/main/resources/config_fakesensors.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_fakesensors_with_storage.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_fakesensors_with_h2.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_fakesensors_with_process.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_fakesensors_group.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_fakesensors_with_auth.json", "storage"});
+        SensorHub.main(new String[] {"src/main/resources/config_empty_sost.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_empty_sost_basicauth.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_empty_sost_with_storage.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_video_tls.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_sat_process.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_metar_with_storage.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_avl_with_storage.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_avl_with_es.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_empty_ble.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_video_sos.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_fakesensors_v4l_sos_sps.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_nexrad_sos.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_oauth_google.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_oauth_github.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_oauth_aws.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_oauth_geoaxis.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_usgs_water.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_fakesensors_sta.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_video_dahua.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_solo.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_uwb.json", "storage"});       
+    }
+
+}
