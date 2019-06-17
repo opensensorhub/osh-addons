b48a0cd7894f59de2573342dfc4d16612f21ac25
Alex Robin <alex.robin@sensiasoft.com>
2019-06-17 14:03:59 +0200

    Added test with H2 DB sampling location index

--- test/sensorhub-test/src/main/java/TestSensorHub.java
+++ test/sensorhub-test/src/main/java/TestSensorHub.java
@@ -16,13 +16,13 @@ public class TestSensorHub
         //SensorHub.main(new String[] {"src/main/resources/config_fakesensors_with_process.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_fakesensors_group.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_fakesensors_with_auth.json", "storage"});
-        SensorHub.main(new String[] {"src/main/resources/config_empty_sost.json", "storage"});
+        //SensorHub.main(new String[] {"src/main/resources/config_empty_sost.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_empty_sost_basicauth.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_empty_sost_with_storage.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_video_tls.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_sat_process.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_metar_with_storage.json", "storage"});
-        //SensorHub.main(new String[] {"src/main/resources/config_avl_with_storage.json", "storage"});
+        SensorHub.main(new String[] {"src/main/resources/config_avl_with_storage.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_avl_with_es.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_empty_ble.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_video_sos.json", "storage"});
