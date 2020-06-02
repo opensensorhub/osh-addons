0e29eb6d0abb0b24b1a8609b53aac696d2054ccf
TheRestOfMe <tony@opensensorhub.org>
2020-06-02 17:33:45 -0500

    Remove Intellisense driver references from test/sensorhub-test

--- test/sensorhub-test/src/main/java/TestSensorHub.java
+++ test/sensorhub-test/src/main/java/TestSensorHub.java
@@ -21,7 +21,7 @@ public class TestSensorHub
         //SensorHub.main(new String[] {"src/main/resources/config_empty_sost_with_storage.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_video_tls.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_sat_process.json", "storage"});
-//        SensorHub.main(new String[] {"src/main/resources/config_metar_with_storage.json", "storage"});
+        SensorHub.main(new String[] {"src/main/resources/config_metar_with_storage.json", "storage"});
 //        SensorHub.main(new String[] {"src/main/resources/config_avl_with_storage.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_avl_with_es.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_empty_ble.json", "storage"});
@@ -37,7 +37,6 @@ public class TestSensorHub
         //SensorHub.main(new String[] {"src/main/resources/config_video_dahua.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_solo.json", "storage"});
         //SensorHub.main(new String[] {"src/main/resources/config_uwb.json", "storage"});       
-    	SensorHub.main(new String[] {"src/main/resources/config_intellisense.json", "storage"});       
     }
 
 }
