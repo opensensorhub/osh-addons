5d852196b18af506569c337841dfaf76099927ee
Alex Robin <alex.robin@sensiasoft.com>
2020-12-04 14:05:40 +0100

    Upgraded MJPEG and MP4 serializers to new async framework

--- sensors/video/sensorhub-service-video/src/main/java/org/sensorhub/impl/service/sos/video/MJPEGSerializer.java
+++ sensors/video/sensorhub-service-video/src/main/java/org/sensorhub/impl/service/sos/video/MJPEGSerializer.java
@@ -14,79 +14,94 @@ Copyright (C) 2012-2016 Sensia Software LLC. All Rights Reserved.
 
 package org.sensorhub.impl.service.sos.video;
 
-import java.io.BufferedOutputStream;
-import java.io.EOFException;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Set;
+import javax.servlet.AsyncContext;
+import javax.servlet.http.HttpServletResponse;
 import net.opengis.swe.v20.DataBlock;
 import net.opengis.swe.v20.DataComponent;
-import org.sensorhub.impl.service.sos.ISOSCustomSerializer;
-import org.sensorhub.impl.service.sos.ISOSDataProvider;
+import org.sensorhub.api.data.DataEvent;
+import org.sensorhub.impl.service.sos.AbstractAsyncSerializer;
+import org.sensorhub.impl.service.sos.ISOSAsyncResultSerializer;
+import org.sensorhub.impl.service.sos.SOSServlet;
+import org.sensorhub.impl.service.swe.RecordTemplate;
 import org.vast.data.DataBlockMixed;
-import org.vast.ows.OWSRequest;
+import org.vast.ows.sos.GetResultRequest;
+import org.vast.ows.sos.SOSException;
 import com.google.common.collect.Sets;
 
 
-public class MJPEGSerializer implements ISOSCustomSerializer
+public class MJPEGSerializer extends AbstractAsyncSerializer<GetResultRequest, DataEvent> implements ISOSAsyncResultSerializer
 {
     private static final String MIME_TYPE_MULTIPART = "multipart/x-mixed-replace; boundary=--myboundary"; 
     private static final byte[] MIME_BOUNDARY_JPEG = new String("--myboundary\r\nContent-Type: image/jpeg\r\nContent-Length: ").getBytes();
     private static final byte[] END_MIME = new byte[] {0xD, 0xA, 0xD, 0xA};
     private static final Set<String> IMG_ARRAY_COMPONENT_NAMES = Sets.newHashSet("img", "videoFrame");
     
-    
+    int imgComponentIdx;
+
+
     @Override
-    public void write(ISOSDataProvider dataProvider, OWSRequest request) throws IOException
+    public void init(SOSServlet servlet, AsyncContext asyncCtx, GetResultRequest req, RecordTemplate resultTemplate) throws SOSException, IOException
     {
-        request.getHttpResponse().addHeader("Cache-Control", "no-cache");
-        request.getHttpResponse().addHeader("Pragma", "no-cache");                    
-        // set multi-part MIME so that browser can properly decode it in an img tag
-        request.getHttpResponse().setContentType(MIME_TYPE_MULTIPART);
-    
-        OutputStream os = new BufferedOutputStream(request.getResponseStream());
+        super.init(servlet, asyncCtx, req);
         
-        // write each record in output stream
-        // skip time stamp to provide raw MJPEG
-        // TODO set timestamp in JPEG metadata
-        DataBlock nextRecord;
-        try
+        if (asyncCtx != null)
         {
-            // get index of image component
-            DataComponent dataStruct = dataProvider.getResultStructure();
-            int imgCompIdx = 0;
-            for (int i = dataStruct.getComponentCount()-1; i >= 0; i--)
-            {
-                if (IMG_ARRAY_COMPONENT_NAMES.contains(dataStruct.getComponent(i).getName()))
-                {
-                    imgCompIdx = i;
-                    break;
-                }
-            }
+            ((HttpServletResponse)asyncCtx.getResponse()).addHeader("Cache-Control", "no-cache");
+            ((HttpServletResponse)asyncCtx.getResponse()).addHeader("Pragma", "no-cache");
             
-            while ((nextRecord = dataProvider.getNextResultRecord()) != null)
+            // set multi-part MIME so that browser can properly decode it in an img tag
+            ((HttpServletResponse)asyncCtx.getResponse()).setContentType(MIME_TYPE_MULTIPART);
+        }
+        
+        // get index of image component
+        DataComponent dataStruct = resultTemplate.getDataStructure();
+        imgComponentIdx = 0;
+        for (int i = dataStruct.getComponentCount()-1; i >= 0; i--)
+        {
+            if (IMG_ARRAY_COMPONENT_NAMES.contains(dataStruct.getComponent(i).getName()))
             {
-                DataBlock frameBlk = ((DataBlockMixed)nextRecord).getUnderlyingObject()[imgCompIdx];
-                byte[] frameData = (byte[])frameBlk.getUnderlyingObject();
-                
-                // write MIME boundary
-                os.write(MIME_BOUNDARY_JPEG);
-                os.write(Integer.toString(frameData.length).getBytes());
-                os.write(END_MIME);
-                
-                os.write(frameData);
-                os.flush();
+                imgComponentIdx = i;
+                break;
             }
         }
-        catch (EOFException e)
+    }
+
+
+    @Override
+    protected void beforeRecords() throws IOException
+    {
+        // nothing to do here
+    }
+
+
+    @Override
+    protected void writeRecord(DataEvent item) throws IOException
+    {
+        // write each record in output stream
+        // skip time stamp and any other field around image data to provide raw MJPEG
+        // TODO set timestamp in JPEG metadata
+        for (var rec: item.getRecords())
         {
-            // this happens if output stream is closed by client
-            // we stop silently in that case
-        }
-        catch (Exception e)
-        {
-            throw new IOException("Error while requesting provider data", e);
-        }
+            DataBlock frameBlk = ((DataBlockMixed)rec).getUnderlyingObject()[imgComponentIdx];
+            byte[] frameData = (byte[])frameBlk.getUnderlyingObject();
+            
+            // write MIME boundary
+            os.write(MIME_BOUNDARY_JPEG);
+            os.write(Integer.toString(frameData.length).getBytes());
+            os.write(END_MIME);
+            
+            os.write(frameData);
+            os.flush();
+        }        
+    }
+
+
+    @Override
+    protected void afterRecords() throws IOException
+    {
+        // nothing to do here
     }
 
 }
