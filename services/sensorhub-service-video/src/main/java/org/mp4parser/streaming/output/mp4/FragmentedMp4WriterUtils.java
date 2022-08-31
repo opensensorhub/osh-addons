3242d2a6fe915a5bcaf226263f1d90dba97cb72b
Christopher S. Dillard <csdillard@gmail.com>
2022-08-31 14:55:35 -0500

    Updates to comments.

--- services/sensorhub-service-video/src/main/java/org/mp4parser/streaming/output/mp4/FragmentedMp4WriterUtils.java
+++ services/sensorhub-service-video/src/main/java/org/mp4parser/streaming/output/mp4/FragmentedMp4WriterUtils.java
@@ -3,11 +3,14 @@ package org.mp4parser.streaming.output.mp4;
 import java.util.Date;
 
 /**
- * This is a helper class that serves as a hack to allow access to some protected members of the FragmentedMp4Writer
- * class.
- * 
- * This is the lesser of two evils, in our opinion. (The alternative is to include the mp4parser library's source in
- * this project and modify the FragmentedMp4Writer class to add a setCreationTime(...) method.)
+ * This is a helper class that allows access to the protected {@link FragmentedMp4Writer#creationTime creationTime}
+ * member of the {@link FragmentedMp4Writer} class. The <code>mp4parser</code> library does not expose a way to
+ * set this value, but we want its value to match the observation time (instead of the default, which is to use the
+ * time when the <code>FragmentedMp4Writer</code> was created).
+ * <p>
+ * Hacking a library in this way is generally not a good idea. But it seems to be a small evil, since it allows us to
+ * use the third-party library as a dependency (rather than forking the library to add this one feature, and/or
+ * including its source in this project).
  */
 public interface FragmentedMp4WriterUtils {
 	public static void setCreationTime(FragmentedMp4Writer fragmentedMp4Writer, Date creationTime) {
