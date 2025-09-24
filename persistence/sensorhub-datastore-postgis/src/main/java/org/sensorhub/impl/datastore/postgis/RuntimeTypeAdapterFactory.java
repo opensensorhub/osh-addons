b229f22b8ec52037c88159c58cf5c035074c3938
earocorn <amavb789@gmail.com>
2025-09-24 10:10:03 -0500

    Remove unnecessary imports

--- persistence/sensorhub-datastore-postgis/src/main/java/org/sensorhub/impl/datastore/postgis/RuntimeTypeAdapterFactory.java
+++ persistence/sensorhub-datastore-postgis/src/main/java/org/sensorhub/impl/datastore/postgis/RuntimeTypeAdapterFactory.java
@@ -25,8 +25,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.time.LocalDateTime;
 import java.util.Map;
 
 public final class RuntimeTypeAdapterFactory<T> implements TypeAdapterFactory {
