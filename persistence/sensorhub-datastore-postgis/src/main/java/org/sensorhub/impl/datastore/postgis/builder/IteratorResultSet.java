8c41a2d6df0c407a618724891b60bd988c4412ed
earocorn <amavb789@gmail.com>
2025-11-25 15:48:50 -0600

    Remove limit cap when using val predicate

--- persistence/sensorhub-datastore-postgis/src/main/java/org/sensorhub/impl/datastore/postgis/builder/IteratorResultSet.java
+++ persistence/sensorhub-datastore-postgis/src/main/java/org/sensorhub/impl/datastore/postgis/builder/IteratorResultSet.java
@@ -59,7 +59,7 @@ public class IteratorResultSet<T> implements Iterator<T> {
                              boolean hasValuePredicate
     ) {
         this.query = !hasValuePredicate ? query : removeSqlLimit(query);
-        this.internalLimit = hasValuePredicate ? 5000 : internalLimit;
+        this.internalLimit = internalLimit;
         this.filterLimit = filterLimit;
         this.parsingFn = parsingFn;
         this.connectionManager = connectionManager;
