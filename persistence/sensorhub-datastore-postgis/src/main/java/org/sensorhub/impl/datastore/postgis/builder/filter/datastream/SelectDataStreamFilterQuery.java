1bed28888176499f7fdaaab512efd0eab4fb2322
Alex Almanza <amavb789@gmail.com>
2026-02-25 13:28:34 -0600

    Fix withCurrentVersion() time filter

--- persistence/sensorhub-datastore-postgis/src/main/java/org/sensorhub/impl/datastore/postgis/builder/filter/datastream/SelectDataStreamFilterQuery.java
+++ persistence/sensorhub-datastore-postgis/src/main/java/org/sensorhub/impl/datastore/postgis/builder/filter/datastream/SelectDataStreamFilterQuery.java
@@ -37,13 +37,16 @@ public class SelectDataStreamFilterQuery extends DataStreamFilterQuery<SelectFil
             else if (temporalFilter.isCurrentTime()) {
                 filterQueryGenerator.addDistinct("(" + tableName + ".data->>'name')");
                 filterQueryGenerator.addDistinct("(" + tableName + ".data->'system@id'->'internalID'->'id')::bigint");
+                filterQueryGenerator.addOrderBy(tableName + ".data->>'name'");
+                filterQueryGenerator.addOrderBy("(" + tableName + ".data->'system@id'->'internalID'->'id')::bigint");
+                filterQueryGenerator.addOrderBy("(" + tableName + ".data->'validTime'->>'begin')::timestamptz DESC ");
                 String sb = "(" +
                         tableName + ".data->'validTime' IS NULL " +
                         "OR (" +
                         tableName + ".data->'validTime'->'begin' IS NOT NULL " +
-                        "AND (" + tableName + ".data->'validTime'->>'begin')::timestamp <= now() " +
+                        "AND (" + tableName + ".data->'validTime'->>'begin')::timestamptz <= now() " +
                         "AND ((" + tableName + ".data->'validTime'->>'end') IS NULL " +
-                        "OR (" + tableName + ".data->'validTime'->>'end')::timestamp >= now())" +
+                        "OR (" + tableName + ".data->'validTime'->>'end')::timestamptz >= now())" +
                         ")" +
                         ")";
                 addCondition(sb);
