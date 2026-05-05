ae31dbf037a0d9d1f5c8b2be423769729a4fc3a4
kyle-fitzp <kyle.fitzpatrick@botts-inc.com>
2026-05-05 13:32:22 -0500

    Remove host override

--- sensors/video/sensorhub-driver-rtmp/src/main/java/org/sensorhub/impl/sensor/rtmp/config/HostType.java
+++ sensors/video/sensorhub-driver-rtmp/src/main/java/org/sensorhub/impl/sensor/rtmp/config/HostType.java
@@ -3,8 +3,8 @@ package org.sensorhub.impl.sensor.rtmp.config;
 public enum HostType {
     UNSPECIFIED("0.0.0.0"),
     LOCALHOST("localhost"),
-    DOCKER_INTERNAL("host.docker.internal"),
-    OVERRIDE("");
+    DOCKER_INTERNAL("host.docker.internal")/*,
+    OVERRIDE("")*/;
 
     public final String host;
 
