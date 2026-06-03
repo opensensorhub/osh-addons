52923decc6c6dc06c509ed5a4bf8dfae04dce843
BillBrown341 <bill.brown@botts-inc.com>
2026-06-03 08:39:59 -0500

    added/updated license header for all code

--- sensors/positioning/sensorhub-driver-nmeaais/src/main/java/org/sensorhub/impl/sensor/nmeaais/Descriptor.java
+++ sensors/positioning/sensorhub-driver-nmeaais/src/main/java/org/sensorhub/impl/sensor/nmeaais/Descriptor.java
@@ -1,4 +1,5 @@
 /***************************** BEGIN LICENSE BLOCK ***************************
+
  The contents of this file are subject to the Mozilla Public License, v. 2.0.
  If a copy of the MPL was not distributed with this file, You can obtain one
  at http://mozilla.org/MPL/2.0/.
@@ -7,7 +8,8 @@
  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
  for the specific language governing rights and limitations under the License.
 
- Copyright (C) 2020-2025 Botts Innovative Research, Inc. All Rights Reserved.
+ Copyright (C) 2026 GeoRobotix Innovative Research. All Rights Reserved.
+
  ******************************* END LICENSE BLOCK ***************************/
 package org.sensorhub.impl.sensor.nmeaais;
 
