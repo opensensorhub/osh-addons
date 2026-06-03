87efdea4833e3cd57c920c79e834b37e846283e2
BillBrown341 <bill.brown@botts-inc.com>
2026-06-03 08:42:20 -0500

    removed unused imports

--- sensors/positioning/sensorhub-driver-nmeaais/src/main/java/org/sensorhub/impl/sensor/nmeaais/outputs/NmeaAisReportInterface.java
+++ sensors/positioning/sensorhub-driver-nmeaais/src/main/java/org/sensorhub/impl/sensor/nmeaais/outputs/NmeaAisReportInterface.java
@@ -12,8 +12,6 @@
 
  ******************************* END LICENSE BLOCK ***************************/
 package org.sensorhub.impl.sensor.nmeaais.outputs;
-import dk.dma.ais.message.AisMessage;
-import dk.dma.ais.message.AisPositionMessage;
 
 public interface NmeaAisReportInterface<T> {
     void setData(T msgTyp, String description);
